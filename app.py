1683db6a9dabfd51c09141dc92d6c9375f9a8820
Moynul Islam <moynul200215@gmail.com>
2026-06-12 15:33:42 +0600

    chore: install flask-sqlalchemy and configure database

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from database.models import db
 
 # Load configurations from .env file
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///travel_deals.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
     # Import blueprints
     from routes import system_bp
---
+++ database/models.py
@@ -0,0 +1,6 @@
+"""Database Models."""
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+
+# Create SQLAlchemy instance
+db = SQLAlchemy()
