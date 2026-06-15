d99c405f19aefdfe7787231bda50afb6f60cd4e9
Moynul Islam <87566898+200215-Moynul-Islam@users.noreply.github.com>
2026-06-15 13:42:48 +0600

    feat: implement search endpoint for deals (#7)
    * feat: implement service method for searching travel deals
    
    * feat: implement a validatation method to validate search query parameters
    
    * feat: implement the endpoint for searching deals
    
    * fix: add validation logic for unknown travel type for search query parameters

--- routes/deal_routes.py
+++ routes/deal_routes.py
@@ -6,7 +6,8 @@ Maps HTTP request to correspoinding business service handlers.
 from http import HTTPStatus, HTTPMethod
 from flask import Blueprint, request, jsonify
 from services import deal_service
-from utils.validators import validate_deal_payload
+from utils import validators
+import logging
 
 deal_bp = Blueprint('deal_bp', __name__)
 
@@ -15,7 +16,7 @@ def add_deal():
     """Add a new travel deal"""
     data = request.get_json()
 
-    validation_errors = validate_deal_payload(data)
+    validation_errors = validators.validate_deal_payload(data)
     if validation_errors:
         return jsonify({
             "status": "error",
@@ -58,3 +59,35 @@ def get_deal_by_id(id : int):
         "status": "success",
         "data": deal
     }), HTTPStatus.OK
+
+@deal_bp.route('/search', methods=[HTTPMethod.GET])
+def search_travel_deals():
+    """Retrieve travel deals filtered by provided search criteria."""
+
+    query_params = request.args.to_dict()
+
+    logging.info(f"Search request received with parameters: {query_params}")
+
+    # Validate the query parameters
+    validation_errors = validators.validate_search_query(query_params)
+    if validation_errors:
+        logging.warning(f"Search request failed validation: {validation_errors}")
+        return jsonify({
+            "status": "error",
+            "errors": validation_errors
+        }), HTTPStatus.BAD_REQUEST
+
+    try:
+        filtered_deals = deal_service.search_deals(query_params)
+        logging.info(f"Search executed successfully.")
+        return jsonify({
+            "status": "success",
+            "data": filtered_deals
+        }), HTTPStatus.OK
+        
+    except Exception as e:
+        logging.error(f"Internal server error: {str(e)}")
+        return jsonify({
+            "status": "error",
+            "message": "Internal server error."
+        }), HTTPStatus.INTERNAL_SERVER_ERROR
--- utils/validators.py
+++ utils/validators.py
@@ -34,6 +34,48 @@ def validate_deal_payload(data: dict) -> list:
         errors.append(f"rating must be between {TravelDealConstants.MIN_RATING} and {TravelDealConstants.MAX_RATING}.")
 
     if data["travel_type"] not in TravelDealConstants.VALID_TYPES:
-        errors.append(f"travel_type must be one of: {', '.join(sorted(TravelDealConstants.VALID_TYPES))}")
+        errors.append(f"travel_type must be one of: {', '.join(TravelDealConstants.VALID_TYPES)}")
+
+    return errors
+
+def validate_search_query(data: dict) -> list:
+    """Validate query parameters for searching travel deals.
+
+    Args:
+        data (dict): Query parameters' dictionary for searching travel deals.
+
+    Returns:
+        list: A list of error message strings. If empty, the payload is valid.
+    """
+
+    errors = []
+
+    destination = data.get("destination")
+    platform = data.get("platform")
+    travel_type = data.get("travel_type")
+
+    # Check for no query parameters
+    if not destination and not platform and not travel_type:
+        errors.append("Search query cannot be empty. Please provide at least one search filter ('destination', 'platform', or 'travel_type').")
+        return errors
+
+    # Check for empty query parameter value
+    if destination is not None and not str(destination).strip():
+        errors.append("Destination search term cannot be empty or whitespace.")
+    if platform is not None and not str(platform).strip():
+        errors.append("Platform search term cannot be empty or whitespace.")
+    if travel_type is not None and not str(travel_type).strip():
+        errors.append("Travel type search term cannot be empty or whitespace.")
+    
+    #check for unknown travel type
+    if travel_type:
+        clean_travel_type = str(travel_type).strip()
+        
+        allowed_travel_types_lower = [t.lower() for t in TravelDealConstants.VALID_TYPES]
+        
+        if clean_travel_type.lower() not in allowed_travel_types_lower:
+            errors.append(
+                f"Unknown travel type: '{clean_travel_type}'. Allowed types are: {', '.join(TravelDealConstants.VALID_TYPES)}."
+            )
 
     return errors
