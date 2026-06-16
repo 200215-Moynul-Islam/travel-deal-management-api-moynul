49ef95132c1330ab2e06e7234fdb5c83b357e2d2
Moynul Islam <87566898+200215-Moynul-Islam@users.noreply.github.com>
2026-06-16 18:29:16 +0600

    feat: implement stat endpoint (#14)
    * feat: implement api request log model and configure a middleware to store api request log
    
    * feat: implement the stat endpoint

--- routes/system_routes.py
+++ routes/system_routes.py
@@ -1,5 +1,8 @@
 from http import HTTPStatus, HTTPMethod
+from venv import logger
 from flask import Blueprint, jsonify
+
+from services import system_service
 
 system_bp = Blueprint('system_bp', __name__)
 
@@ -11,3 +14,19 @@ def health_check():
         "status": "success",
         "message": "Application is running."
     }), HTTPStatus.OK
+
+@system_bp.route('/stats', methods=[HTTPMethod.GET])
+def get_stats():
+    """Retrieve statistics of API request."""
+    logger.info(f"Get stats request received.")
+    try:
+        analytics_data = system_service.get_api_analytics()
+        logger.info(f"Get stats request executed successfully.")
+        return jsonify(analytics_data), HTTPStatus.OK
+        
+    except Exception as e:
+        logger.error(f"Internal server error")
+        return jsonify({
+            "status": "error",
+            "error": "Internal server error."
+        }), HTTPStatus.INTERNAL_SERVER_ERROR
